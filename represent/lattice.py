5f2461626892d398fd8eabcbc7e419fd9152bc9f
Axel Gagge <axel.gagge@fysik.su.se>
2018-09-17 13:36:33 +0200

    Corrected error in simple_chain function and added a function for a composite chain with unit cell

--- represent/lattice.py
+++ represent/lattice.py
@@ -20,6 +20,29 @@ def simple_chain(N, periodic=False):
     links += [{'indices': [i, i+1], 'tags': set(["nn", "nn_left"])} for i in range(N-1)]
     links += [{'indices': [i+1, i], 'tags': set(["nn", "nn_right"])} for i in range(N-1)]
     if periodic == True:
-        links.append({'indices': [N, 0], 'tags': set(["nn", "nn_left"])})
-        links.append({'indices': [0, N], 'tags': set(["nn", "nn_right"])})
+        links.append({'indices': [N-1, 0], 'tags': set(["nn", "nn_left"])})
+        links.append({'indices': [0, N-1], 'tags': set(["nn", "nn_right"])})
+    return Lattice(kinds, links)
+
+def unit_cell_chain(cell, N_cells, periodic=False):
+    n_cell = len(cell)
+    kinds = []
+    links = []
+    for i in range(N_cells):
+        for j, kind in enumerate(cell):
+            idx = n_cell*i+j
+            kinds.append(kind)
+            nn_tag = "nn_" + kind
+            links.append({'indices': [idx],
+                          'tags': set(["onsite"])})
+            links.append({'indices': [idx, idx+1],
+                          'tags': set([nn_tag, nn_tag + "_left"])})
+            links.append({'indices': [idx+1, idx],
+                          'tags': set([nn_tag, nn_tag + "_right"])})
+    if periodic == True:
+        N = n_cell*N_cells
+        kind = cell[-1]
+        nn_tag = "nn_" + kind
+        links.append({'indices': [N-1, 0], 'tags': set([nn_tag, nn_tag + "_left"])})
+        links.append({'indices': [0, N-1], 'tags': set([nn_tag, nn_tag + "_right"])})
     return Lattice(kinds, links)
